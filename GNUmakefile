1966e64d83d71ce092f0a6fcc55ef704d9f736ed
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-12-05 19:13:36 +0000

    Adding zenwebserver to trunk. Reviewed by Joseph.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.WebScale@51306 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ GNUmakefile
@@ -0,0 +1,61 @@
+######################################################################
+#
+# Copyright 2011 Zenoss, Inc.  All Rights Reserved.
+#
+######################################################################
+
+#==================================
+NGINXVERSION=1.0.10
+UPSTREAMFAIRVERSION=7171df84c5
+#==================================
+
+HERE=$(PWD)
+LIBDIR=$(HERE)/lib
+BUILDDIR=$(HERE)/build
+TARGETDIR=$(HERE)/ZenPacks/zenoss/WebScale
+PREFIXDIR=$(ZENHOME)
+BINDIR=$(TARGETDIR)/bin
+NGINX=$(LIBDIR)/nginx-$(NGINXVERSION).tar.gz
+NGINXDIR=$(BUILDDIR)/nginx-$(NGINXVERSION)
+UPSTREAMFAIR=$(LIBDIR)/nginx-upstream-fair-$(UPSTREAMFAIRVERSION).tar.gz
+UPSTREAMFAIRDIR=$(BUILDDIR)/nginx-upstream-fair-$(UPSTREAMFAIRVERSION)
+
+default: egg
+
+egg:
+	@python setup.py bdist_egg
+
+%/.d:
+	@mkdir -p $(@D)
+	@touch $@
+
+$(NGINXDIR)/.d: $(BUILDDIR)/.d
+	@cd $(BUILDDIR) && tar xzf $(NGINX)
+	@touch $(@)
+
+$(UPSTREAMFAIRDIR)/.d: $(BUILDDIR)/.d
+	@cd $(BUILDDIR) && tar xzf $(UPSTREAMFAIR)
+	@touch $(@)
+
+$(BINDIR)/nginx: $(BINDIR)/.d $(PREFIXDIR)/.d $(UPSTREAMFAIRDIR)/.d $(NGINXDIR)/.d
+	@echo "Building nginx..."
+	@cd $(NGINXDIR) && \
+		./configure \
+       		--prefix=$(PREFIXDIR) \
+			--sbin-path=$(BINDIR)/nginx \
+			--http-client-body-temp-path=$(PREFIXDIR)/var/nginx_temp \
+			--error-log-path=$(PREFIXDIR)/log/nginx/error.log \
+			--with-http_ssl_module \
+			--with-http_stub_status_module \
+			--add-module=$(UPSTREAMFAIRDIR) \
+		&& make && make install 
+
+build: $(BINDIR)/nginx
+
+clean:
+	@rm -rf $(BUILDDIR) dist temp
+	@rm -rf $(TARGETDIR)/lib $(BINDIR)
+	@rm -rf *.egg-info
+	@find . -name *.pyc | xargs rm -f
+
+.PRECIOUS: %/.d
---
+++ ZenPacks/zenoss/WebScale/__init__.py
@@ -0,0 +1,177 @@
+######################################################################
+#
+# Copyright 2007 Zenoss, Inc.  All Rights Reserved.
+#
+######################################################################
+import Globals
+import os
+import sys
+import time
+import shutil
+import logging
+import subprocess
+from Products.ZenUtils.Utils import zenPath
+from Products.ZenModel.ZenPack import ZenPackBase
+
+
+log = logging.getLogger("zen.WebScale")
+
+
+def get_port():
+    port = '8080'
+    with open(zenPath('etc', 'zope.conf'), 'r') as f:
+        for line in f:
+            if line.strip().startswith('address'):
+                port = line.split()[-1]
+                break
+    return port
+
+
+def replace_zopectl_in_daemons_txt():
+    daemonstxt = zenPath('etc', 'daemons.txt')
+    if os.path.exists(daemonstxt):
+        with open(daemonstxt, 'r') as f:
+            newfile = f.read().replace('zopectl', 'zenwebserver')
+        with open(daemonstxt, 'w') as f:
+            f.write(newfile)
+
+
+def is_using_many_zopes():
+    # This is relatively naive, but errs on the side of caution. Also I don't
+    # know what else we can do.
+    daemonstxt = zenPath('etc', 'daemons.txt')
+    if not os.path.exists(daemonstxt):
+        # Using the default daemons, so no more zopes
+        return False
+    with open(daemonstxt, 'r') as f:
+        count = 0
+        for line in f:
+            if 'zope' in line:
+                count += 1
+                if count > 1:
+                    return True
+    return False
+
+
+def use_zenwebserver():
+    """
+    Make the change to force zenwebserver
+    """
+    log.info("Replacing zopectl with zenwebserver")
+    subprocess.call([zenPath('bin', 'zenglobalconf'), '-u', 'webserverctl=zenwebserver'])
+
+
+def use_zopectl():
+    """
+    Undo zenwebserver and use the default
+    """
+    log.info("Removing zenwebserver and restoring zopectl")
+    subprocess.call([zenPath('bin', 'zenglobalconf'), '-r', 'webserverctl'])
+
+
+class ZenPack(ZenPackBase):
+
+    def _executable(self, *path):
+        """
+        Make specified file executable.
+        """
+        fname = self.path(*path)
+        os.chmod(fname, 0755)
+
+    def _symlink(self, fro, to):
+        """
+        Symlink from a path within this zenpack to a path under $ZENHOME.
+        Pass in tuples of path segments.
+        """
+        src = self.path(*fro)
+        dest = zenPath(*to)
+        if os.path.lexists(dest):
+            os.remove(dest)
+        os.symlink(src, dest)
+
+    def _copy(self, fro, to):
+        """
+        Copy from a path within this zenpack to a path under $ZENHOME.
+        Do NOT overwrite existing files.
+        Pass in tuples of path segments.
+        """
+        src = self.path(*fro)
+        dest = zenPath(*to)
+        if not os.path.exists(dest):
+            shutil.copyfile(src, dest)
+
+    def install(self, dmd):
+        log.info("Installing zenwebserver and nginx to $ZENHOME/bin")
+        # Create zenwebserver symlink
+        self._symlink(('zenwebserver',), ('bin', 'zenwebserver'))
+        self._executable('zenwebserver')
+
+        # Create bin/nginx symlink
+        self._symlink(('bin', 'nginx'), ('bin', 'nginx'))
+        self._executable('bin', 'nginx')
+
+        log.info("Installing nginx configuration to $ZENHOME/etc")
+
+        port = get_port()
+        log.info("Detected Zope is using port %s" % port)
+        # Replace strings in nginx.conf
+        with open(self.path('nginx.conf'), 'r') as f, open(self.path('nginx.conf.tmp'), 'w') as f2:
+            f2.write(f.read()
+                     .replace('<<INSTANCE_HOME>>', zenPath())
+                     .replace('<<PORT>>', port))
+
+        # Create mime.types symlink
+        self._symlink(('mime.types',), ('etc', 'mime.types'))
+
+        # Copy in nginx configs
+        self._copy(('nginx.conf.tmp',), ('etc', 'nginx.conf'))
+        self._copy(('nginx-zope.conf',), ('etc', 'nginx-zope.conf'))
+
+        # Clean up
+        os.remove(self.path('nginx.conf.tmp'))
+
+        try:
+            os.mkdir(zenPath('etc', 'zope'))
+            log.info("Created multi-Zope config directory")
+        except OSError:
+            # Already exists
+            pass
+
+        try:
+            os.mkdir(zenPath('log', 'nginx'))
+            log.info("Created nginx log directory")
+        except OSError:
+            # Already exists
+            pass
+
+        if is_using_many_zopes():
+            log.warn("Already using multiple Zopes; not switching to use zenwebserver by default.")
+        else:
+            replace_zopectl_in_daemons_txt()
+            use_zenwebserver()
+            log.info("Attempting to shut down running zopectl")
+            subprocess.call([zenPath('bin', 'zopectl'), 'stop'])
+            time.sleep(4) # Wait for it to shut down
+            log.info("Starting zenwebserver")
+            # subprocess.call([zenPath('bin', 'zenwebserver'), 'start'])
+        log.info("Installed successfully. Run 'zenwebserver start' to start the UI server.")
+
+
+    def remove(self, dmd, leaveObjects=False):
+        try:
+            subprocess.call([zenPath('bin', 'zenwebserver'), 'stop'])
+        except OSError:
+            log.warn('Unable to shut down zenwebserver')
+        if not leaveObjects: # upgrade
+            use_zopectl()
+            log.info("Uninstalling zenwebserver and nginx")
+            for f in ('bin/zenwebserver', 'bin/nginx', 'etc/mime.types'):
+                try:
+                    os.remove(zenPath(f))
+                except OSError:
+                    log.info("%s doesn't exist, so not removing" % zenPath(f))
+            try:
+                shutil.rmtree(zenPath('etc', 'zope'))
+            except OSError:
+                log.info("Multi-zope config directory doesn't exist, so not removing")
+            log.info("Uninstalled zenwebserver. You will need to start Zope.")
