e1f37334bfd95d2e46c982aa1c5a7f0b07dc95c5
jplouis <jplouis@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-05-04 21:09:36 +0000

    fixes ZEN-1432 add renderurl support to hubs. reviewed by jcausey
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.WebScale@57821 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/WebScale/distributedcollector.py
+++ ZenPacks/zenoss/WebScale/distributedcollector.py
@@ -8,7 +8,7 @@ from subprocess import check_output, CalledProcessError
 from zope.interface import implements
 from zope.component import adapts
 from Products.ZenUtils.Utils import zenPath
-from ZenPacks.zenoss.DistributedCollector.DCUtils import CollectorConfFactory
+from ZenPacks.zenoss.DistributedCollector.DCUtils import CollectorConfFactory, HubConfFactory
 from ZenPacks.zenoss.DistributedCollector.interfaces import IRemoteRenderUrlProvider
 from Products.ZenWidgets.messaging import IMessageSender, WARNING
 
@@ -16,6 +16,9 @@ import logging
 log = logging.getLogger("zen.webscale_dc")
 
 _ZENRENDER_PORT = 8091
+_HUBZENRENDER_PORT = 8090
+_COLLECTOR_TYPE = 'collector'
+_HUB_TYPE='hub'
 
 class WebScaleRenderUrlProvider(object):
     implements(IRemoteRenderUrlProvider)
@@ -28,14 +31,24 @@ class WebScaleRenderUrlProvider(object):
     def get_render_url(self):
         return '/remote-collector/%s' % self.confFactory._monitorName
 
+class WebScaleHubRenderUrlProvider(object):
+    implements(IRemoteRenderUrlProvider)
+    adapts(HubConfFactory)
+
+    def __init__(self, confFactory):
+        self.confFactory = confFactory
+
+    def get_render_url(self):
+        return '/remote-hub/%s' % self.confFactory._monitorName
+
 
 def onCollectorInstalled(ob, event):
-    errorMsg = _reconfigureNginx(ob.id, ob.hostname)
+    errorMsg = _reconfigureNginx(ob.id, ob.hostname, _COLLECTOR_TYPE)
     if errorMsg:
         IMessageSender(ob).sendToBrowser('Error', errorMsg, WARNING)
 
 def onCollectorDeleted(ob, event):
-    filePath = _dcNginxConfPath(ob.id)
+    filePath = _dcNginxConfPath(ob.id, _COLLECTOR_TYPE)
     try:
         os.remove(filePath)
     except Exception:
@@ -44,15 +57,36 @@ def onCollectorDeleted(ob, event):
 def onCollectorUpdated(ob, event):
     changes = event.propertyChanges
     if changes.has_key('hostname') or changes.has_key('renderurl') :
-        errorMsg = _reconfigureNginx(ob.id, ob.hostname)
+        errorMsg = _reconfigureNginx(ob.id, ob.hostname, _COLLECTOR_TYPE)
         if errorMsg:
             IMessageSender(ob).sendToBrowser('Error', errorMsg, WARNING)
 
-def _reconfigureNginx(id, host):
+def onHubInstalled(ob, event):
+    errorMsg = _reconfigureNginx(ob.id, ob.hostname, _HUB_TYPE)
+    if errorMsg:
+        IMessageSender(ob).sendToBrowser('Error', errorMsg, WARNING)
+
+def onHubDeleted(ob, event):
+    filePath = _dcNginxConfPath(ob.id, _HUB_TYPE)
+    try:
+        os.remove(filePath)
+    except Exception:
+        pass
+
+def onHubUpdated(ob, event):
+    changes = event.propertyChanges
+    if changes.has_key('hostname') or changes.has_key('renderurl') :
+        errorMsg = _reconfigureNginx(ob.id, ob.hostname, _HUB_TYPE)
+        if errorMsg:
+            IMessageSender(ob).sendToBrowser('Error', errorMsg, WARNING)
+
+
+
+def _reconfigureNginx(id, host, dctype):
     errorMsg = None
     try:
         #overwrite nginx conf for this collector
-        _writeDcConf(id, host)
+        _writeDcConf(id, host, dctype)
     except Exception as e:
         errorMsg = "Could not write Nginx configuration for collector %s: %s" % (id , e)
         log.warn(errorMsg)
@@ -68,8 +102,8 @@ def _reconfigureNginx(id, host):
     return errorMsg
 
 _NGINX_CONF_TMPL = """
-location ^~ /remote-collector/{collectorId}/ {{
-    rewrite ^/remote-collector/(.*)$ /$1 break;
+location ^~ /remote-{collectorType}/{collectorId}/ {{
+    rewrite ^/remote-{collectorType}/(.*)$ /$1 break;
     proxy_pass http://{collectorHost}:{collectorPort};
 
     proxy_set_header        Host    $host;
@@ -78,17 +112,21 @@ location ^~ /remote-collector/{collectorId}/ {{
     }}
 """
 
-def _writeDcConf(id, host):
-    confValues = {'collectorId': id,
+def _writeDcConf(id, host, dctype):
+    port =_ZENRENDER_PORT
+    if dctype == _HUB_TYPE:
+        port = _HUBZENRENDER_PORT
+    confValues = {'collectorType': dctype,
+                  'collectorId': id,
                   'collectorHost': host,
-                  'collectorPort': _ZENRENDER_PORT}
+                  'collectorPort': port}
     conf = _NGINX_CONF_TMPL.format(**confValues)
-    filePath = _dcNginxConfPath(id)
+    filePath = _dcNginxConfPath(id, dctype)
     with open(filePath, 'w') as confFile:
         confFile.write(conf)
 
-def _dcNginxConfPath(id):
-    confName = 'nginx-dc-{collectorId}.conf'.format(collectorId=id)
+def _dcNginxConfPath(id, dctype):
+    confName = 'nginx-dc-{dctype}-{collectorId}.conf'.format(dctype=dctype, collectorId=id)
     filePath = zenPath('etc', confName)
     return filePath
 
